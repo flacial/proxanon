156f82c4b4eee9727cc371621beb6a676937a37f
Flacial <ruthemiumwithdiamond@protonmail.com>
2023-06-08 20:00:50 +0400

    Change file extension to js

---
+++ index.js
@@ -0,0 +1,45 @@
+import express from "express";
+import geohash from "ngeohash";
+import {
+  uniqueNamesGenerator,
+  adjectives,
+  colors,
+  animals,
+} from "unique-names-generator";
+
+/**
+ * @type {Object.<string, Set>}
+ */
+const grids = {};
+const app = express();
+
+app.use(express.json());
+app.use(express.static("public"));
+
+app.post("/api/initial", (req, res) => {
+  const { lat, lon } = req.body;
+  // geohash of length 5 is <= 4.89km X 4.89km
+  const hash = geohash.encode(lat, lon).slice(0, 5);
+
+  const randomName = uniqueNamesGenerator({
+    dictionaries: [adjectives, colors, animals],
+    separator: "-",
+  }); // big-red-donkey
+
+  let grid = grids[hash];
+
+  if (!grid) {
+    grids[hash] = new Set();
+    grid = grids[hash];
+  }
+
+  grid.add({
+    randomName,
+  });
+
+  res.status(200).send({ hash, username: randomName });
+});
+
+app.listen(3000, () => {
+  console.log("Example app listening on port 3000");
+});
