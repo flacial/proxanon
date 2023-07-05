931849c75d0cd27c26cd707799379c8db61d3e1d
Flacial <ruthemiumwithdiamond@protonmail.com>
2023-07-05 07:19:15 +0400

    Add fonts

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -9,6 +9,10 @@ export default {
         darkGray: "#C2C2C2",
         blackGray: "#191919",
       },
+      fontFamily: {
+        display: ["AgencyFB", "sans-serif"],
+        body: ["Proxima Nova", "sans-serif"],
+      },
     },
   },
   plugins: [],
