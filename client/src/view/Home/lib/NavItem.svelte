f39a37cc152b99061decc275f41319997fdf08fb
Flacial <ruthemiumwithdiamond@protonmail.com>
2023-07-09 16:05:14 +0400

    Create partial Home page

---
+++ client/src/view/Home/lib/NavItem.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+  export let imageName: string = ''
+  export let alt: string = ''
+  export let active: boolean = false
+  export let onClick: () => void = () => {}
+</script>
+
+<div class="grid justify-items-center">
+  <div
+    class={`w-2 h-2 ${
+      active
+        ? 'shadow-[0_0_5px_3px_rgba(255,86,29,0.2)] orange-gradient'
+        : 'bg-zinc-700'
+    } rounded-full mb-2 transition-shadow`}
+  />
+  <button
+    class="p-2 bg-secondaryGray rounded-full w-fit hover:opacity-90 cursor-pointer focus:outline-none focus:ring focus:ring-orange-300 active:opacity-90"
+    on:click={onClick}
+  >
+    <img
+      class="filter brightness-75"
+      src={`/src/assets/icons/${imageName}`}
+      {alt}
+      width="31"
+      height="31"
+    />
+  </button>
+</div>
