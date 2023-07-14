3ff989ab68ee8c377b3dc7554e9b36ecd1266c14
Flacial <ruthemiumwithdiamond@protonmail.com>
2023-07-14 16:58:28 +0400

    Add Chat component to Home page

--- client/src/view/Home/Home.svelte
+++ client/src/view/Home/Home.svelte
@@ -1,7 +1,8 @@
 <script lang="ts">
   import { onDestroy, onMount } from 'svelte'
-  import Navbar from './lib/Navbar.svelte'
   import { NavItemType } from './type/nav'
+  import Navbar from './lib/Navbar.svelte'
+  import Chat from '../Chat/Chat.svelte'
 
   const capitalize = (str: string) => str.charAt(0).toUpperCase() + str.slice(1)
 
@@ -25,15 +26,15 @@
 
 <div class="grid grid-cols-[auto,1fr]">
   <Navbar bind:activeNavItem />
-  {#if activeNavItem === NavItemType.Chat}
-    <div>
-      <h1>Chat</h1>
-    </div>
-  {:else if activeNavItem === NavItemType.Post}
-    <h1>Post</h1>
-  {:else if activeNavItem === NavItemType.Setting}
-    <h1>Setting</h1>
-    <!-- {:else}
-    <p>Not implemented yet</p> -->
-  {/if}
+  <div class="bg-[#262626] p-10 flex flex-col gap-6 max-h-[100dvh]">
+    {#if activeNavItem === NavItemType.Chat}
+      <Chat />
+    {:else if activeNavItem === NavItemType.Post}
+      <h1>Post</h1>
+    {:else if activeNavItem === NavItemType.Setting}
+      <h1>Setting</h1>
+      <!-- {:else}
+  <p>Not implemented yet</p> -->
+    {/if}
+  </div>
 </div>
