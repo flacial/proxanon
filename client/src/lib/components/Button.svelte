7e31874329e97cd5ae0f54944ed21cef9df254b1
Flacial <ruthemiumwithdiamond@protonmail.com>
2023-07-14 16:57:53 +0400

    Create a global Button component

---
+++ client/src/lib/components/Button.svelte
@@ -0,0 +1,11 @@
+<button
+  on:focus
+  on:click
+  on:mouseover
+  on:mouseenter
+  on:mouseleave
+  {...$$restProps}
+  class={`${$$restProps.class} hover:opacity-90 cursor-pointer focus:outline-none focus:ring focus:ring-orange-300 active:opacity-90`}
+>
+  <slot />
+</button>
