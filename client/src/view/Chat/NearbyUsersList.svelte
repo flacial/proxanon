b218078f9d2ff192bf1d39d19a5b4d57862b12f6
Flacial <ruthemiumwithdiamond@protonmail.com>
2023-07-17 14:00:57 +0400

    Filter user from nearby users list

--- client/src/view/Chat/NearbyUsersList.svelte
+++ client/src/view/Chat/NearbyUsersList.svelte
@@ -1,7 +1,10 @@
 <script lang="ts">
   import HelpImage from '../../lib/assets/icons/help.png'
   import Button from '../../lib/components/Button.svelte'
+  import { getUser } from '../../utils/userdata'
   import UserCard from './lib/UserCard.svelte'
+
+  const user = getUser()
 
   export let gridUsers: string[] = []
   export let chattingWith: string = ''
@@ -20,10 +23,12 @@
   </div>
   <div class="grid gap-4 overflow-auto max-h-[100dvh]">
     {#each gridUsers as username}
-      <UserCard
-        username={username.replaceAll('-', ' ')}
-        onStartChat={() => (chattingWith = username)}
-      />
+      {#if username !== user.username}
+        <UserCard
+          username={username.replaceAll('-', ' ')}
+          onStartChat={() => (chattingWith = username)}
+        />
+      {/if}
     {/each}
   </div>
 </div>
