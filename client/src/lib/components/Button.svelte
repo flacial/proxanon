46836c86e119717d54ec067c2802fe827e2b4e05
Flacial <ruthemiumwithdiamond@protonmail.com>
2023-07-17 15:30:38 +0400

    Learned about Svelte magic

--- client/src/lib/components/Button.svelte
+++ client/src/lib/components/Button.svelte
@@ -5,7 +5,7 @@
   on:mouseenter
   on:mouseleave
   {...$$restProps}
-  class={`${$$restProps.class} hover:opacity-90 cursor-pointer focus:outline-none focus:ring focus:ring-orange-300 active:opacity-90`}
+  class="{$$restProps.class} hover:opacity-90 cursor-pointer focus:outline-none focus:ring focus:ring-orange-300 active:opacity-90"
 >
   <slot />
 </button>
--- client/src/view/Home/lib/NavItem.svelte
+++ client/src/view/Home/lib/NavItem.svelte
@@ -7,11 +7,9 @@
 
 <div class="grid justify-items-center">
   <div
-    class={`w-2 h-2 ${
-      active
-        ? 'shadow-[0_0_5px_3px_rgba(255,86,29,0.2)] orange-gradient'
-        : 'bg-zinc-700'
-    } rounded-full mb-2 transition-shadow`}
+    class="w-2 h-2 {active
+      ? 'shadow-[0_0_5px_3px_rgba(255,86,29,0.2)] orange-gradient'
+      : 'bg-zinc-700'} rounded-full mb-2 transition-shadow"
   />
   <button
     class="p-2 bg-secondaryGray rounded-full w-fit hover:opacity-90 cursor-pointer focus:outline-none focus:ring focus:ring-orange-300 active:opacity-90"
